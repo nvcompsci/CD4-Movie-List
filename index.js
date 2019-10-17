6f31d2af6a7fac5bccf74cbe1ac3c1cf55f46c91
John Word <jword@sylvaniaschools.local>
2019-10-17 14:26:51 -0400

    GET /movies and create index.html

--- index.js
+++ index.js
@@ -2,10 +2,17 @@
 const express = require("express")
 const app = express()
 
+let movies = ["Star Wars Episode IV","Anchorman"]
+
 //Define callback function for GET request on route /hello
 app.get("/hello", (req, res) => {
     console.log(req.ip)
     res.send("Hello good sir")
+})
+
+app.get("/movies", (req, res) => {
+    console.log("GET on /movies")
+    res.send(movies)
 })
 
 //Start listening on port 3000, callback function
